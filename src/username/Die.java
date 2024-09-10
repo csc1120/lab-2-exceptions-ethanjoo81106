3d5099ce3f2cf793afbcc09f4e721e6c64e7c27e
jooe <jooe@msoe.edu>
2024-09-10 11:26:45 -0500

    Commit 2 - Die class

--- src/username/Die.java
+++ src/username/Die.java
@@ -2,11 +2,43 @@
  * Course: CSC1020
  * Lab 2 - Exceptions
  * Die class
- * Name: FIXME
- * Last Updated: FIXME
+ * Name: Ethan
+ * Last Updated: Joo
  */
 package username;
 
+import java.util.Random;
+
+/**
+ * The die object stores current value and number of sides
+ */
 public class Die {
+    private int currentValue;
+    private final int numSides;
+    private Random random;
+
+    /**
+     * Main constructor for die class
+     * @param numSides input of # of sides
+     */
+    public Die(int numSides){
+        this.numSides = numSides;
+    }
+
+    /**
+     * returns the value of the die
+     * @return currentValue of die
+     */
+    public int getCurrentValue(){
+        return currentValue;
+    }
+
+    /**
+     * rolls the die to a random number
+     */
+    public void roll(){
+        currentValue = random.nextInt(numSides-1) + 1;
+    }
+
 
 }
